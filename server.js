e4836cba08f5ddb2d7b353c54b1a6862b3944b5b
mehdicode <kalan.mahdi@yahoo.com>
2017-07-25 17:49:10 -0700

    s2

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var mongoose = require('mongoose');
 var request = require("request");
 var cheerio = require("cheerio");
 var bodyParser = require("body-parser");
+var Comment = require("./models/Comment.js");
+var Article = require("./models/Article.js");
+mongoose.Promise = Promise;
 
 // Initialize Express
 var app = express();
@@ -31,101 +34,132 @@ var collections = ["scrapedData"];
 
 var db = mongojs(databaseUrl, collections);
 db.on("error", function(error) {
-  console.log("Database Error:", error);
+    console.log("Database Error:", error);
 });
 
+// -----------------------------------------------------------------------------------------------
+    request("http://www.latimes.com/", function(error, response, html) {
+
+        // Load the HTML into cheerio and save it to a variable
+        // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+        var $ = cheerio.load(html);
+
+        // An empty array to save the data that we'll scrape
+        var result = {};
 
 
-app.get("/all", function(req, res) {
-    db.news.find({}, function(err, data) {
-    // Log any errors if the server encounters one
-                var hbsObject = {
-                data: data
-            };
-    if (err) {
-      console.log(err);
-    }
-    // Otherwise, send the result of this query to the browser
-    else {
-    	
-      res.render("index", hbsObject);
-    }
-  });
-  
-});
+        // Select each element in the HTML body from which you want information.
+        // NOTE: Cheerio selectors function similarly to jQuery's selectors,
+        // but be sure to visit the package's npm page to see how it works
+        $("a.trb_outfit_relatedListTitle_a").each(function(i, element) {
 
-app.post("/all", function(req, res) {
-	console.log(req);
-    var data = [];
-    data.push({
+            result.link = $(element).attr("href");
+            result.title = $(element).text();
 
-      titleId:req.body.pId,
-      comment:req.body.comment
+            var entry = new Article(result);
+
+            // Now, save that entry to the db
+            entry.save(function(err, doc) {
+
+                // Log any errors
+                if (err) {
+                    console.log(err);
+                }
+                // Or log the doc
+                else {
+
+                    console.log(doc);
+
+                }
+            });
+
+        });
 
     });
-    db.comments.insert(data);
-    res.end();
+    // ------------------------------------------------------------------------------
 
-  
+
+
+app.get("/", function(req, res) {
+
+          Article.find({}, function(error, doc) {
+        var hbsObject = {
+            data: doc
+        };
+        // Log any errors
+        if (error) {
+          console.log(error);
+        }
+        // Or send the doc to the browser as a json object
+        else {
+          res.render("index", hbsObject);
+        }
+      });
+
+
+
 });
-// "_id": mongojs.ObjectId(req.params.id)
-app.post("/comments", function(req, res) {
-	
-  db.comments.find({"titleId":req.body.tid}, function(err, data) {
+
+app.post("/", function(req, res) {
+
+    var data = {};
+    data.body = req.body.comment;
+
+    var newComment = new Comment(data);
+
+    // And save the new note the db
+    newComment.save(function(error, doc) {
+        // Log any errors
+        if (error) {
+            console.log(error);
+        }
+        // Otherwise
+        else {
+              Article.findOneAndUpdate({ "_id": req.body.pId }, { $push: { "comment": doc._id }}, { new: true }, function(err, newdoc) {
+        // Send any errors to the browser
+        if (err) {
+          res.send(err);
+        }
+        // Or send the newdoc to the browser
+        else {
+          res.redirect("/");
+        }
+      })
+
+        }
+    });
    
-    var hbsObject = {
-      data: data
-    };
-    
-    if (err) {
-      console.log(err);
+
+});
+
+app.post("/comments", function(req, res) {
+
+
+  Article.find({ "_id": req.body.tid })
+  // ..and populate all of the notes associated with it
+  .populate("comment")
+  // now, execute our query
+  .exec(function(error, doc) {
+    // Log any errors
+    if (error) {
+      console.log(error);
     }
-    // Otherwise, send the result of this query to the browser
+    // Otherwise, send the doc to the browser as a json object
     else {
-    	
-      res.render("comments", hbsObject);
+      res.json(doc);
     }
   });
 
 
-});
-
-
-
- request("http://www.latimes.com/", function(error, response, html) {
-
-  // Load the HTML into cheerio and save it to a variable
-  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
-  var $ = cheerio.load(html);
-
-  // An empty array to save the data that we'll scrape
-  var results = [];
-
-  // Select each element in the HTML body from which you want information.
-  // NOTE: Cheerio selectors function similarly to jQuery's selectors,
-  // but be sure to visit the package's npm page to see how it works
-  $("a.trb_outfit_relatedListTitle_a").each(function(i, element) {
-
-    var link = $(element).attr("href");
-    var title = $(element).text();
-
-    // Save these results in an object that we'll push into the results array we defined earlier
-    results.push({
-      title: title,
-      link: "http://www.latimes.com"+link
-    });
-    db.news.insert(results)
-  });
-
 
 });
+
+
 
 
 // Listen on port 3000
 app.listen(3000, function() {
-  console.log("App running on port 3000!");
+    console.log("App running on port 3000!");
 });
-
-
 
 mongoose.connect("mongodb://heroku_15cwths1:j0305mb0nbelu6cnjer3vrevr2@ds115583.mlab.com:15583/heroku_15cwths1")
